afd8d458cfe66f6bd13840d481f2cd1520fe31b7
Вадим <matveevvs174rus@gmail.com>
2024-07-05 15:26:05 +0500

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Button from "../Button/Button";
 
 const Header = () => {
+    const tg = window.Telegram.WebApp;
+
+    const onClose = () => {
+        tg.close()
+    }
+
     return (
         <div className={'header'}>
             <button onClick={onClose}>Закрыть</button>
             <span className={'username'}>
+                {initDataUnsafe?.user?.username}
             </span>
 
         </div>
