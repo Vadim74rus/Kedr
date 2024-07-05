b34b1afa3c956e3968a99021fda3c8313119c01d
Вадим <matveevvs174rus@gmail.com>
2024-07-05 18:10:38 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Button/Header/Header";
 
+function App() {
+  const { onToggleButton, onClose, tg } = useTelegram();
 
-function  App() {
-    const {onToggleButton, tg} = useTelegram();
+  useEffect(() => {
+    tg.ready();
+  }, []);
 
-    useEffect(() => {
-        tg.ready()
-    }, []);
-    const onClose = () => {
-        tg.close()
-    }
-
-    return (
-        <div className="App">
-            <Header />
-            <button onClick={onToggleButton}>toggle</button>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <button onClick={onToggleButton}>Toggle</button>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
 }
 
 export default App;
